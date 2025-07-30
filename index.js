9a249915602b8adb9ea4152b248cef3719221f64
Kewal <ghosalkar.kewal9@gmail.com>
2025-07-30 18:29:41 +0530

    Completed gameLoop

--- index.js
+++ index.js
@@ -9,16 +9,201 @@ const healthEl = document.getElementById("health-el")
 //Drawing Sizes
 const bbThickness = 5
 const bbSize = 400
+let bbStartX = canvEl.width/2 - bbSize/2
+let bbStartY = canvEl.height/2 - bbSize/2
+let bbEndX = canvEl.width/2 - bbSize/2 + bbSize
+let bbEndY = canvEl.height/2 - bbSize/2 + bbSize
 
 //Player Object
 let player = {
-    size: 20,
+    size: 23,
     x: canvEl.width/2,
     y: canvEl.height/2,
-    speed: 0.4,
+    speed: 0.38,
     health: 100,
-    healthOld: 0
+    healthOld: 0,
+    coolTime: 5,
+    coolTimeStr: 1000,
+    coolDown: false,
+    coolAnim: false
 }
+
+//Attacks
+let bar = []
+
+let atkNo = 4
+
+let bullets = {
+    straight(xRel, yRel, xVel, yVel) {
+        return {
+            size: 20,
+            x: xRel,
+            y: yRel,
+            speedx: xVel * deltaTime,
+            speedy: yVel * deltaTime,
+            laser: false,
+
+            updateSpeed() {
+                // Do nothing
+            }
+        }
+    },
+    speed(xRel, yRel, xVel, yVel) {
+        return {
+            size: 20,
+            x: xRel,
+            y: yRel,
+            speedx: xVel * deltaTime,
+            speedy: yVel * deltaTime,
+            spawn: fc,
+            laser: true,
+
+            updateSpeed() {
+                this.speedx *= 1+(fc-this.spawn)/5000
+                this.speedy *= 1+(fc-this.spawn)/5000
+            }
+        }
+    }
+}
+
+let aco = 4
+
+let attacks = [
+    //Circle
+    function() {
+        const del = 300
+        if ((fc-aco) > del && fc > 240) {
+            let xx
+            let yy
+
+            aco = fc
+
+            while (!((xx < bbStartX || xx > bbEndX) || (yy < bbStartY || yy > bbEndY))) {
+                xx = Math.random() * canvEl.width
+                yy = Math.random() * canvEl.height
+            }
+
+            bar.push(bullets.straight(xx, yy, 0.005, 0.005))
+            bar.push(bullets.straight(xx, yy, -0.005, 0.005))
+            bar.push(bullets.straight(xx, yy, 0.005, -0.005))
+            bar.push(bullets.straight(xx, yy, -0.005, -0.005))
+            bar.push(bullets.straight(xx, yy, 0.007, 0))
+            bar.push(bullets.straight(xx, yy, 0, 0.007))
+            bar.push(bullets.straight(xx, yy, -0.007, 0))
+            bar.push(bullets.straight(xx, yy, 0, -0.007))
+        }
+    },
+    //Slow Snipe
+    function() {
+        const del = 150
+        if ((fc-aco) > del && fc > 240) {
+            let xx
+            let yy
+
+            aco = fc
+
+            while (!((xx < bbStartX || xx > bbEndX) || (yy < bbStartY || yy > bbEndY))) {
+                xx = Math.random() * canvEl.width
+                yy = Math.random() * canvEl.height
+            }
+
+            let dx = player.x - xx
+            let dy = player.y - yy
+            let norm = Math.sqrt(dx*dx + dy*dy)
+
+
+            //console.log([xx, yy, dx/norm, dy/norm])
+
+            bar.push(bullets.straight(xx, yy, (dx/norm) * 0.01, (dy/norm) * 0.01))
+        }
+    },
+    //Speed Snipe
+    function() {
+        const del = 500
+        if ((fc-aco) > del && fc > 240) {
+            let xx
+            let yy
+
+            aco = fc
+
+            while (!((xx < bbStartX || xx > bbEndX) || (yy < bbStartY || yy > bbEndY))) {
+                xx = Math.random() * canvEl.width
+                yy = Math.random() * canvEl.height
+            }
+
+            let dx = player.x - xx
+            let dy = player.y - yy
+            let norm = Math.sqrt(dx*dx + dy*dy)
+
+
+            //console.log([xx, yy, dx/norm, dy/norm])
+
+            bar.push(bullets.speed(xx, yy, (dx/norm) * 0.005, (dy/norm) * 0.005))
+            bar.push(bullets.speed(xx, yy, (dx/norm) * 0.004, (dy/norm) * 0.004))
+            bar.push(bullets.speed(xx, yy, (dx/norm) * 0.003, (dy/norm) * 0.003))
+            bar.push(bullets.speed(xx, yy, (dx/norm) * 0.002, (dy/norm) * 0.002))
+            bar.push(bullets.speed(xx, yy, (dx/norm) * 0.001, (dy/norm) * 0.001))
+        }
+    },
+    //Random Snipe
+    function() {
+        const del = 100
+        if ((fc-aco) > del && fc > 240) {
+            let xx
+            let yy
+
+            aco = fc
+
+            while (!((xx < bbStartX || xx > bbEndX) && (yy < bbStartY || yy > bbEndY))) {
+                xx = Math.random() * canvEl.width
+                yy = Math.random() * canvEl.height
+            }
+
+            let dx = Math.random()*canvEl.width - xx
+            let dy = Math.random()*canvEl.height - yy
+            let norm = Math.sqrt(dx*dx + dy*dy)
+
+
+            //console.log([xx, yy, dx/norm, dy/norm])
+
+            bar.push(bullets.speed(xx, yy, (dx/norm) * 0.005, (dy/norm) * 0.005))
+            bar.push(bullets.speed(xx, yy, (dx/norm) * 0.004, (dy/norm) * 0.004))
+            bar.push(bullets.speed(xx, yy, (dx/norm) * 0.003, (dy/norm) * 0.003))
+            bar.push(bullets.speed(xx, yy, (dx/norm) * 0.002, (dy/norm) * 0.002))
+            bar.push(bullets.speed(xx, yy, (dx/norm) * 0.001, (dy/norm) * 0.001))
+        }
+    },
+    //Speed Snipe Double
+    function() {
+        const del = 500
+        if ((fc-aco) > del && fc > 240) {
+            for (let i = 0; i < 2; i++) {
+                let xx
+                let yy
+
+                aco = fc
+
+                while (!((xx < bbStartX || xx > bbEndX) || (yy < bbStartY || yy > bbEndY))) {
+                    xx = Math.random() * canvEl.width
+                    yy = Math.random() * canvEl.height
+                }
+
+                let dx = player.x - xx
+                let dy = player.y - yy
+                let norm = Math.sqrt(dx*dx + dy*dy)
+
+
+                //console.log([xx, yy, dx/norm, dy/norm])
+
+                bar.push(bullets.speed(xx, yy, (dx/norm) * 0.005, (dy/norm) * 0.005))
+                bar.push(bullets.speed(xx, yy, (dx/norm) * 0.004, (dy/norm) * 0.004))
+                bar.push(bullets.speed(xx, yy, (dx/norm) * 0.003, (dy/norm) * 0.003))
+                bar.push(bullets.speed(xx, yy, (dx/norm) * 0.002, (dy/norm) * 0.002))
+                bar.push(bullets.speed(xx, yy, (dx/norm) * 0.001, (dy/norm) * 0.001))
+            }
+        }
+    }
+]
 
 //Input
 let keys = []
@@ -29,11 +214,11 @@ let currTime = Date.now()
 let deltaTime = 1
 const timeSamples = 30
 
-
 //Setup
-let fc = 0
+let fc = 1
 
 //#region Event Listners
+{
 window.addEventListener('resize', function(event) {
     setCanvas()
 })
@@ -45,6 +230,7 @@ document.addEventListener("keydown", function(event) {
 document.addEventListener("keyup", function(event) {
     keys[event.key] = false
 })
+}
 
 //#region Functions
 function setTime() {
@@ -53,8 +239,11 @@ function setTime() {
     oldTime = currTime
 
     if (fc == timeSamples) {
-        player.speed = Math.floor(deltaTime/timeSamples * player.speed)
+        player.speed = (deltaTime/timeSamples * player.speed)
+        player.coolTimeStr = Math.floor(player.coolTimeStr/(deltaTime/timeSamples))
         console.log("Player Speed: " + player.speed)
+        console.log("Player Cooldown: " + player.coolTimeStr)
+        player.coolTime = player.coolTimeStr
     }
 }
 
@@ -62,10 +251,15 @@ function setCanvas() {
     canvEl.width = window.innerWidth - 50
     canvEl.height = window.innerHeight - 90
 
+    bbStartX = canvEl.width/2 - bbSize/2
+    bbStartY = canvEl.height/2 - bbSize/2
+    bbEndX = canvEl.width/2 - bbSize/2 + bbSize
+    bbEndY = canvEl.height/2 - bbSize/2 + bbSize
+
     ctx.fillStyle = "#ffffff"
-    ctx.fillRect(canvEl.width/2 - bbSize/2, canvEl.height/2 - bbSize/2, bbSize, bbSize)
+    ctx.fillRect(bbStartX, bbStartY, bbSize, bbSize)
     ctx.fillStyle = "#000000"
-    ctx.fillRect((canvEl.width/2 - bbSize/2) + bbThickness, (canvEl.height/2 - bbSize/2) + bbThickness, bbSize - bbThickness*2, bbSize - bbThickness*2)
+    ctx.fillRect(bbStartX + bbThickness, bbStartY + bbThickness, bbSize - bbThickness*2, bbSize - bbThickness*2)
 
     player.x = canvEl.width/2
     player.y = canvEl.height/2
@@ -73,19 +267,19 @@ function setCanvas() {
 
 function updatePlayer() {
     if (keys["ArrowUp"]) {
-        if (player.y-player.speed > (canvEl.height/2 - bbSize/2 + bbThickness))    
+        if (player.y-player.speed > bbStartY + bbThickness)    
             player.y -= player.speed
     }
     if (keys["ArrowDown"]) {
-        if (player.y + player.speed < (canvEl.height/2 - bbSize/2 + bbThickness) + (bbSize - 2*bbThickness) - player.size)
+        if (player.y + player.speed < bbEndY - bbThickness - player.size)
             player.y += player.speed
     }
     if (keys["ArrowLeft"]) {
-        if (player.x-player.speed > (canvEl.width/2 - bbSize/2 + bbThickness))    
+        if (player.x-player.speed > bbStartX + bbThickness)    
             player.x -= player.speed 
     }
     if (keys["ArrowRight"]) {
-        if (player.x + player.speed < (canvEl.width/2 - bbSize/2 + bbThickness) + (bbSize - 2*bbThickness) - player.size)
+        if (player.x + player.speed < bbEndX - bbThickness - player.size)
             player.x += player.speed 
     }
 
@@ -93,36 +287,115 @@ function updatePlayer() {
         player.healthOld = player.health
         healthEl.textContent = "Health: " + player.health
     }
+
+    if (player.coolTime > 0 && player.coolDown)    player.coolTime--
+    else if (player.coolTime <= 0) {
+        player.coolTime = player.coolTimeStr
+        player.coolDown = false
+    }
+}
+
+function updatebullet() {
+    for (let i = 0; i < bar.length; i++) {
+        bar[i].updateSpeed()
+        bar[i].x += bar[i].speedx
+        bar[i].y += bar[i].speedy
+    }
 }
 
 function drawScreen() {
     ctx.clearRect(0, 0, canvEl.width, canvEl.height);
 
     ctx.fillStyle = "#ffffff"
-    ctx.fillRect(canvEl.width/2 - bbSize/2, canvEl.height/2 - bbSize/2, bbSize, bbSize)
+    ctx.fillRect(bbStartX, bbStartY, bbSize, bbSize)
     ctx.fillStyle = "#000000"
-    ctx.fillRect((canvEl.width/2 - bbSize/2) + bbThickness, (canvEl.height/2 - bbSize/2) + bbThickness, bbSize - bbThickness*2, bbSize - bbThickness*2)
+    ctx.fillRect(bbStartX + bbThickness, bbStartY + bbThickness, bbSize - bbThickness*2, bbSize - bbThickness*2)
 
-    ctx.fillStyle = "#ff0000"
-    ctx.fillRect(player.x, player.y, player.size, player.size)
+    
+    
+    if (!player.coolDown) {
+        ctx.fillStyle = "#ff0000"
+        ctx.fillRect(player.x, player.y, player.size, player.size)
+    }
+    else if (player.coolTime > 0 && player.coolAnim && player.coolTime % 10 == 0) {
+        player.coolAnim = false
+    }
+    else if (player.coolTime > 0 && !player.coolAnim && player.coolTime % 10 == 0) {
+        ctx.fillStyle = "#ff0000"
+        ctx.fillRect(player.x, player.y, player.size, player.size)
+        player.coolAnim = true
+    }
+
+    ctx.fillStyle = "#ffffff"
+
+    for (let i = 0; i < bar.length; i++) {
+        if (bar[i].x < 0 || bar[i].x > canvEl.width || bar[i].y < 0 || bar[i].y > canvEl.height) {
+            bar.splice(i, 1)
+            continue
+        }
+
+        if (bar[i].laser) {
+            ctx.beginPath();
+            ctx.moveTo(bar[i].x + bar[i].size/2, bar[i].y + bar[i].size/2);
+            ctx.lineTo(bar[i].x + bar[i].speedx * 10000, bar[i].y + bar[i].speedy * 10000);
+            ctx.strokeStyle = "#ff3333";
+            ctx.lineWidth = 1;
+            ctx.stroke();
+        }
+        ctx.fillRect(bar[i].x, bar[i].y, bar[i].size, bar[i].size)
+
+        let a = bar[i].x + bar[i].size > player.x
+        let b = bar[i].x < player.x + player.size  //collision from right
+        let c = bar[i].y + bar[i].size > player.y //Collision from top
+        let d = bar[i].y < player.y + player.size  //Collisioj from bottom
+
+        let x = [a, b, c, d]
+
+        if (a && b && c && d) {
+            if (!player.coolDown) {
+                player.health -= 10
+                player.coolDown = true
+            }
+        }
+    }
 }
 
 function update() {
+    if (player.health > 0) {
+        
+        if (fc <= timeSamples) {
+            setTime()
+            fc++
+        }
+        else {
+            fc += Math.floor(deltaTime/timeSamples)
+        }
 
-    if (fc <= timeSamples) {
-        setTime()
+        if (fc > (atkNo + 1) * 5000) {
+            atkNo++
+            console.log(fc)
+        }
+
+        attacks[atkNo%attacks.length]()
+        updatePlayer()
+        updatebullet()
+        drawScreen()
+
+        if (fc % 100 == 0)  player.health++
+        
+
+        requestAnimationFrame(update)
+        //console.log(atkNo)
     }
-
-    updatePlayer()
-
-    drawScreen()
-
-    fc++
-
-    requestAnimationFrame(update)
+    else {
+        ctx.clearRect(0, 0, canvEl.width, canvEl.height);
+        healthEl.textContent = "Health: 0"
+    }
 }
 
 
-//#region Main Code
+//#region Main
 setCanvas()
+// bar.push(createStrBullet(10, 10))
+// bar.push(createStrBullet(20, 20))
 update()
